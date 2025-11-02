9e4856c655ac07ae3604ba23ed3ee0a939fd012f
srikanth-suthari <suthari.srikanth@outlook.com>
2025-11-02 17:37:39 +0530

    scripting

---
+++ 07-commands-execution.sh
@@ -0,0 +1,15 @@
+#!bin/bash
+
+#executing some commands in shell scripting
+
+TIME=$(date +%H-%M-%S)
+
+echo "The time is: $TIME"
+
+# this script takes 10 sec to print the date
+sleep 10
+echo
+DATE=$(date +%d-%m-%Y)
+
+echo "The date is: $DATE"
+
