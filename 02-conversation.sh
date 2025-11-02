8b3e7a61f55c0b9de3f005acbf5645a04c97887d
srikanth-suthari <suthari.srikanth@outlook.com>
2025-11-02 12:12:30 +0530

    scripting

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -2,7 +2,7 @@
 
 #some conversation between two people
 
-echo "Hi Srikanth, How are you?"
+echo "Sridhar: Hi Srikanth, How are you?"
 echo "Srikanth: Hello, I am fine. How about you?"
 echo "Sridhar: I am also doing good."
 echo "Srikanth: Okay that's good to hear from you Sridhar, Bye..."
---
+++ 03-variables.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+
+#variables in bash scripting
+#var_name=value
+
+person1=Sridhar
+person2=Srikanth
+
+#using the defined variables in some conversation statements
+#usage of variables $var_name
+
+echo "$person1: Hi $person2, How are you?"
+echo "$person2: Hello $person1, I am good and How are you $person1."
