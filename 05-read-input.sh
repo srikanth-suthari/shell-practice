915aed3a894aab36925dd9b9e67d264c04e3ed99
srikanth-suthari <suthari.srikanth@outlook.com>
2025-11-02 14:08:37 +0530

    scripting

---
+++ 05-read-input.sh
@@ -0,0 +1,12 @@
+#!bin/bash
+
+#reading user input from terminal using read function
+
+echo "Please enter you card number"
+read card_number
+echo "Your card number is: $card_number"
+echo
+echo "Please enter the PIN"
+#this is the recommeded way of hiding passwords while user input
+read -s PIN_NUM		#this hides displaying the input while user entering
+echo $PIN_NUM
