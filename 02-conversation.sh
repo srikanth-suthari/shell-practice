0358ae5f202ed8c53dbd96b3dca504a24793487e
srikanth-suthari <suthari.srikanth@outlook.com>
2025-11-02 12:04:10 +0530

    some conversation

---
+++ 02-conversation.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+
+#some conversation between two people
+
+echo "Hi Srikanth, How are you?"
+echo "Srikanth: Hello, I am fine. How about you?"
+echo "Sridhar: I am also doing good."
+echo "Srikanth: Okay that's good to hear from you Sridhar, Bye..."
