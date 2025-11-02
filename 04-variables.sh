66d6e7b4357810af0416ad4c145a165396800069
srikanth-suthari <suthari.srikanth@outlook.com>
2025-11-02 13:43:57 +0530

    scripting

---
+++ 04-variables.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+
+#Arguments in scripting
+
+#defining variables
+person1=$1
+person2=$2
+
+#accessing the variables
+echo "$person1: Hello, $person2"
+echo "$person2: Hi, $person1"
+
+#here i'm passing the arguements from the cli for person1 and person2
